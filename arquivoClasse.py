203563c6a813648652fe8e45828c81baebd2465b
BieldoJT <113216641+BieldoJT@users.noreply.github.com>
2024-04-05 21:37:53 -0300

    Add files via upload

--- arquivoClasse.py
+++ arquivoClasse.py
@@ -3,22 +3,46 @@ import datetime
 import os
 
 class Questionario:
-    def __init__(self,nome_arquivo='questionario.csv'):
+    def __init__(self, nome_arquivo='questionario.csv', nome_arquivo_backup = 'backup.csv'):
         self.nome_arquivo = nome_arquivo
+        self.nome_arquivo_backup = nome_arquivo_backup
+        self.backup = []
         self.perguntas = [
             "Pergunta 1? (1 - Sim, 2 - Não, 3 - Não sei): ",
             "Pergunta 2? (1 - Sim, 2 - Não, 3 - Não sei): ",
             "Pergunta 3? (1 - Sim, 2 - Não, 3 - Não sei): ",
             "Pergunta 4? (1 - Sim, 2 - Não, 3 - Não sei): "
         ]
-        self.backup =  []
-        
+        self.linha_backup = []
         if os.path.exists(nome_arquivo):
-            self.respostas = pd.read_csv(nome_arquivo).to_dict('records')
+            self.respostas = pd.read_csv(nome_arquivo)
             self.num_linhas = len(self.respostas)
         else:
-            self.respostas = []
+            self.respostas = pd.DataFrame(columns=['ID', 'Idade', 'Gênero', 'Resposta 1', 'Resposta 2', 'Resposta 3', 'Resposta 4', 'Data/Hora'])
             self.num_linhas = 0
+
+    
+    def salva_backup(self):
+        if os.path.exists(self.nome_arquivo_backup):
+            self.backup = pd.read_csv(self.nome_arquivo_backup)
+            new_df_backup = pd.DataFrame(self.linha_backup)
+            self.backup = pd.concat([self.backup, new_df_backup], ignore_index=True).drop_duplicates()
+            
+        else:
+            
+            self.backup = pd.DataFrame(self.linha_backup)
+            
+        self.backup.to_csv(self.nome_arquivo_backup, index=False) 
+            
+        print(self.backup)
+
+    
+    def maior_id(self):
+        if os.path.exists(self.nome_arquivo):
+            maior_id = max(self.respostas['ID'].tolist())+1
+        else:
+            maior_id=1
+        return maior_id
     
     def coletar_informacoes(self):
         while True:
@@ -39,8 +63,8 @@ class Questionario:
             else:
                 print("Gênero inválido. Por favor, insira 'M' para masculino ou 'F' para feminino.")
 
-        respostas = {'ID': self.num_linhas+1,'Idade': idade, 'Gênero': genero}
-        self.num_linhas += 1
+        respostas = {'ID': self.maior_id(),'Idade': idade, 'Gênero': genero}
+        
         
         for i, pergunta in enumerate(self.perguntas, start=1):
             while True:
@@ -59,8 +83,26 @@ class Questionario:
                 respostas[f'Resposta {i}'] = 'não sabe'
             
         respostas['Data/Hora'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.respostas.append(respostas)
+        self.respostas = pd.concat([self.respostas, pd.DataFrame([respostas])], ignore_index=True)
+        self.num_linhas +=1
         return True
+    
+    
+    def remove_linha(self):
+        id = int(input('Digite o ID a ser deletado: '))
+        if id in self.respostas['ID'].tolist():
+            self.backup = self.respostas[self.respostas['ID'] == id]
+            self.respostas = self.respostas[self.respostas['ID'] != id]
+            self.respostas.to_csv(self.nome_arquivo, index=False)
+            self.num_linhas -= 1
+            print(f'Linha com ID {id} removida do arquivo CSV.')
+            print(self.linha_backup)
+            self.salva_backup()
+            print(self.respostas)
+            return True
+        else:
+            print("ID não encontrado.")
+            return False
 
     def escrever_csv(self):
         if os.path.exists(self.nome_arquivo):
@@ -79,21 +121,4 @@ class Questionario:
         print(df)
         print(f'Numero de linhas: {self.num_linhas}')
 
-    def remove_linha(self):
-        id = int(input('Digite o ID a ser deletado'))
-        for i, resposta in enumerate(self.respostas):
-            if resposta['ID'] == id:
-                self.backup = self.respostas[i]
-                del self.respostas[i]
-                self.num_linhas -= 1
-                df = pd.DataFrame(self.respostas)
-                
-                
-                
-                
-                
-                print(f'\nessa linha foi deletada {self.backup}')
-                del self.respostas[i]
-                self.num_linhas -= 1
-                return True
-        return False
+    
